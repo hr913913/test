d9c3c0db6225d18d1c52ae6a19c7aca6f4425780
hr913913 <55601929+hr913913@users.noreply.github.com>
2023-03-28 01:04:30 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 for i in {1..10}; do
   ZONE=$(gcloud compute zones list --format='value(name)' --filter='region:us' --sort-by=compute.googleapis.com/pricing/1cpu+1gb | grep -v ZONE_TO_EXCLUDE | head -n 1)
-  gcloud compute instance-templates create "test-$i" --template test --zone=$ZONE
+  gcloud compute instance-templates create  test --zone=$ZONE
 done
